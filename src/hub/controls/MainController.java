734a91730e6398b330937305c9c7c93fe70592c8
Soyoun Kim <makeajourney@gmail.com>
2016-06-04 15:37:43 +0900

    기본 경로로 접속하면 메인으로 redirect, 메인에서 세션 확인하여 로그인 정보 없으면 로그인으로 redirect

--- src/hub/controls/MainController.java
+++ src/hub/controls/MainController.java
@@ -2,13 +2,23 @@ package hub.controls;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
+import hub.vo.User;
+
 public class MainController implements Controller {
 
 	@Override
 	public String execute(Map<String, Object> model) throws Exception {
 		// TODO Auto-generated method stub
+		HttpSession session = (HttpSession) model.get("session");
+		User user = (User) session.getAttribute("user");
 		
-		return "main.jsp";
+		if (user == null) {
+			return "redirect:./auth/login.do";
+		} else {			
+			return "main.jsp";
+		}
 	}
 
 }
