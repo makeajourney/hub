7590c8cce5c7a4d134bf3a9878db7f92af57f6f1
Soyoun Kim <makeajourney@gmail.com>
2016-06-09 01:43:59 +0900

    유저가 추가한 키워드를 디비에서 가져오는 메소드 getUserKeyword

--- src/hub/dao/HubDao.java
+++ src/hub/dao/HubDao.java
@@ -3,9 +3,12 @@ package hub.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.sql.DataSource;
 
+import hub.vo.Keyword;
 import hub.vo.User;
 
 
@@ -44,6 +47,36 @@ public class HubDao {
 			try { if (rs != null) rs.close();} catch (Exception e){}
 			try { if (stmt != null) stmt.close();} catch (Exception e){}
 			try {if (connection != null) connection.close(); } catch(Exception e) {}
+		}
+	}
+	
+	// 사용자가 추가한 단어들을 가져옴.
+	public List<Keyword> getUserKeyword(String id) throws Exception {
+		Connection connection = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			connection = ds.getConnection();
+			stmt = connection.prepareStatement(
+					"SELECT WORD FROM KEYWORD K,USER_KEYWORD UK" +
+					" WHERE K.NO = UK.NO AND UK.NO = (SELECT NO FROM USER WHERE ID=?)");
+			stmt.setString(1, id);
+			rs = stmt.executeQuery();
+			
+			ArrayList<Keyword> keywords = new ArrayList<Keyword>();
+			
+			while (rs.next()) {
+				keywords.add(new Keyword()
+						.setWord(rs.getString("KEYWORD")));
+			}
+			return keywords;
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			try { if (rs != null) rs.close();} catch(Exception e) {}
+			try { if (stmt != null) stmt.close();} catch(Exception e) {}
+			try { if (connection != null) connection.close(); } catch (Exception e) {}
 		}
 	}
 }
